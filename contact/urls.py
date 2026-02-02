376ea8fbabd547cd44e323dd415ab2e29eb873aa
Nadsson <nadssonkauea@gmail.com>
2026-02-02 11:34:51 -0300

    Adicionando o header, contact.html e a contact view

--- contact/urls.py
+++ contact/urls.py
@@ -6,8 +6,13 @@ app_name = 'contact'
 
 urlpatterns = [
     path(
+        '<int:contact_id>/',
+        views.contact,
+        name='contact'
+        ),
+    path(
         '',
         views.index,
         name='index'
-        )
+        ),
 ]
