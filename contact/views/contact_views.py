0059ce9bd690474b830d000788977f3f1b8a8c18
Nadsson <nadssonkauea@gmail.com>
2026-02-02 11:45:48 -0300

    adicionando partials e site_title

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,7 +8,8 @@ def index(request):
         .filter(show=True)\
         .order_by('id')[:10  ]
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Contatos | '
     }
 
     return render(
@@ -19,8 +20,12 @@ def index(request):
 def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    site_title = f'{single_contact.first_name} {single_contact.last_name} |'
+
     context = {
-        'contact': single_contact
+        'contact': single_contact,
+        'site_title': site_title
     }
 
     return render(
