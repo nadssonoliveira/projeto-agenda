58c6668a3c12f01672b8a1f1fa1213ac7c05e0a3
Nadsson <nadssonkauea@gmail.com>
2026-02-08 18:30:45 -0300

    Adicionando validações com add_error e método clean

--- contact/forms.py
+++ contact/forms.py
@@ -5,6 +5,17 @@ from contact import models
 
 class ContactForm(forms.ModelForm):
 
+    first_name = forms.CharField(
+        widget= forms.TextInput(
+            attrs={
+                'class':'classe-a classse-b',
+                'placeholder': 'Aqui veio do init'
+            }
+            ),
+            label='Primeiro Nome',
+            help_text='Texto para ajudar o usuário',
+    )    
+
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -29,10 +40,31 @@ class ContactForm(forms.ModelForm):
 
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
-        
-        self.add_error(
+        cleaned_data = self.cleaned_data
+
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name ==  last_name:
+            msg = ValidationError('Primeiro nome não pode ser igual ao ultimo.', code='invalid')
+
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+
+
+        return super().clean()
+
+
+    def clean_first_name(self):
+
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
             'first_name', ValidationError(
-                'Nome inválido', code='invalid'
+                'Erro de validação', code='invalid'
             )
         )
+
+        return first_name
