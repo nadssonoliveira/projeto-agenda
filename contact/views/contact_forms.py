fbeffb52babd8b4271a5c6e2cd3dfbda952ec07e
Nadsson <nadssonkauea@gmail.com>
2026-02-16 20:50:37 -0300

    adicionando owner  aos contatos

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -22,7 +22,9 @@ def create(request):
         }
 
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update', contact_id=contact.pk)
             
 
@@ -47,7 +49,7 @@ def create(request):
 @login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
+        Contact, pk=contact_id, show=True, owner=request.user
         )
     form_action = reverse('contact:update', args=(contact_id,))
 
@@ -86,7 +88,7 @@ def update(request, contact_id):
 @login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
+        Contact, pk=contact_id, show=True, owner=request.user
         )
     confirmation = request.POST.get('confirmation', 'no')
     
